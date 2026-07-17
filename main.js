dab891af6575b48ad3b64db5b1c9b48e1fdf3a32
thegurt <owensthegurt@gmail.com>
2026-07-17 16:17:55 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,26 @@
+const createBtn =
+document.getElementById("createBtn");
+
+
+const joinBtn =
+document.getElementById("joinBtn");
+
+
+
+createBtn.onclick = () => {
+
+
+    console.log("Creating party...");
+
+
+};
+
+
+
+joinBtn.onclick = () => {
+
+
+    console.log("Joining party...");
+
+
+};
