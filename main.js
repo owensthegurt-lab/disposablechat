3161a53a59308fde3a1754447f5aa62d7ac3c97a
thegurt <owensthegurt@gmail.com>
2026-07-17 16:21:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+const menu =
+document.getElementById("menu");
+
+
 const createBtn =
 document.getElementById("createBtn");
 
@@ -7,20 +11,142 @@ document.getElementById("joinBtn");
 
 
 
+function generateCode(){
+
+    const chars =
+    "ABCDEFGHJKLMNPQRSTUVWXYZ123456789";
+
+
+    let code = "";
+
+
+    for(let i = 0; i < 5; i++){
+
+        code += chars[
+            Math.floor(
+                Math.random() * chars.length
+            )
+        ];
+
+    }
+
+
+    return code;
+
+}
+
+
+
+
 createBtn.onclick = () => {
 
 
-    console.log("Creating party...");
+    const code =
+    generateCode();
+
+
+
+    menu.innerHTML = `
+
+
+        <h1>
+            Party Created
+        </h1>
+
+
+        <p>
+            Share this code with your friends
+        </p>
+
+
+        <h2>
+            ${code}
+        </h2>
+
+
+        <button id="startBtn">
+            Start Chat
+        </button>
+
+
+    `;
+
+
+
+    document
+    .getElementById("startBtn")
+    .onclick = () => {
+
+
+        console.log(
+            "Starting chat:",
+            code
+        );
+
+
+    };
 
 
 };
 
 
 
+
 joinBtn.onclick = () => {
 
 
-    console.log("Joining party...");
+    menu.innerHTML = `
+
+
+        <h1>
+            Join Party
+        </h1>
+
+
+        <p>
+            Enter party code
+        </p>
+
+
+        <input 
+        id="codeInput"
+        placeholder="XXXXX"
+        maxlength="5"
+        >
+
+
+        <br>
+
+
+        <button id="joinConfirm">
+            Join
+        </button>
+
+
+    `;
+
+
+
+    document
+    .getElementById("joinConfirm")
+    .onclick = () => {
+
+
+        const code =
+        document
+        .getElementById("codeInput")
+        .value
+        .toUpperCase();
+
+
+
+        console.log(
+            "Joining:",
+            code
+        );
+
+
+    };
 
 
 };
